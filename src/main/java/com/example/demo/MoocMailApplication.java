a7ce9654f2fa8e44f9b96b69758bf295eadb3f8f
explorren <986554261@qq.com>
2017-09-19 13:05:23 +0800

    test

---
+++ src/main/java/com/example/demo/MoocMailApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoocMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoocMailApplication.class, args);
+	}
+}
